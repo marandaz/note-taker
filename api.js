d599de1ed9918820ca85898f6eb139e4383c6fb5
marandaz <mrzwies@gmail.com>
2020-12-04 19:15:28 -0600

    more route fixes

--- api.js
+++ api.js
@@ -1,36 +1,35 @@
-const confirmedNotes = require("./db/db.json")
+const saveNote = require("./db/db.json")
 const fs = require("fs");
 
 module.exports = app => {
-  app.get("/api/confirmedNotes", function (req, res) {
-    return res.json(confirmedNotes)
+  app.get("/api/notes", function (req, res) {
+    return res.json(saveNote)
   })
 
-  app.post("/api/confirmedNotes", function (req, res) {
+  app.post("/api/notes", function (req, res) {
 
     const newNote = req.body;
-    (confirmedNotes.length === 0) ? newNote.id = 0 : newNote.id = confirmedNotes.length;
-    confirmedNotes.push(newNote);
-    const noteString = JSON.stringify(confirmedNotes);
+    (saveNote.length === 0) ? newNote.id = 0 : newNote.id = saveNote.length;
+    saveNote.push(newNote);
+    const noteString = JSON.stringify(saveNote);
 
     fs.writeFile("./db/db.json", noteString, function (err) {
       if (err) throw err;
-      console.log("Note Sent!");
+      console.log("Note captured!");
       res.send();
     });
   })
 
-  app.delete("/api/confirmedNotes/:id", function (req, res) {
-    confirmedNotes.splice(req.params.id, 1);
-    if (confirmedNotes) {
-      for (let i = 0; i < confirmedNotes.length; i++) {
-        confirmedNotes[i].id = i;
+  app.delete("/api/notes/:id", function (req, res) {
+    saveNote.splice(req.params.id, 1);
+    if (saveNote) {
+      for (let i = 0; i < saveNote.length; i++) {
+        saveNote[i].id = i;
       };
     };
 
-    fs.writeFile("./db/db.json", JSON.stringify(confirmedNotes), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(saveNote), (err) => {
       if (err) throw err;
-      console.log("note" + req.params.id + " deleted");
       res.send();
     });
   });
--- htmlRoutes.js
+++ htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = app => {
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
   
   app.get("/notes", function (req, res) {
